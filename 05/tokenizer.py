8f22ea5aea6fb139e8d15da798ac1dad277f9579
Yuichi Takeuchi <yuichi.takeuchi@takeyuweb.co.jp>
2022-12-20 02:51:00 +0000

    シーケンスへの変換

--- 05/tokenizer.py
+++ 05/tokenizer.py
@@ -4,10 +4,15 @@ from tensorflow.keras.preprocessing.text import Tokenizer
 
 sentences = [
     'Today is a sunny day',
-    'Today is a rainy day'
+    'Today is a rainy day',
+    'Is it sunny today?'
 ]
 
 tokenizer = Tokenizer(num_words=100)    # トークンの最大数を指定してインスタンス化
 tokenizer.fit_on_texts(sentences)
 word_index = tokenizer.word_index
-print(word_index)
+
+sequences = tokenizer.texts_to_sequences(sentences)
+
+print(sequences)
+# [[1, 2, 3, 4, 5], [1, 2, 3, 6, 5], [2, 7, 4, 1]]
