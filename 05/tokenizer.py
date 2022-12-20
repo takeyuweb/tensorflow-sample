29a32494b80036012780d1ac42ee240e435b9449
Yuichi Takeuchi <yuichi.takeuchi@takeyuweb.co.jp>
2022-12-20 03:00:04 +0000

    語彙外

--- 05/tokenizer.py
+++ 05/tokenizer.py
@@ -8,7 +8,8 @@ sentences = [
     'Is it sunny today?'
 ]
 
-tokenizer = Tokenizer(num_words=100)    # トークンの最大数を指定してインスタンス化
+# OOV=Out-Of-Vocabulary
+tokenizer = Tokenizer(num_words=100, oov_token="<OOV>")
 tokenizer.fit_on_texts(sentences)
 word_index = tokenizer.word_index
 
@@ -16,3 +17,18 @@ sequences = tokenizer.texts_to_sequences(sentences)
 
 print(sequences)
 # [[1, 2, 3, 4, 5], [1, 2, 3, 6, 5], [2, 7, 4, 1]]
+
+text_data = [
+    'Today is a snowy day',
+    'Will it be rainy tomorrow?'
+]
+word_index = tokenizer.word_index
+sequences = tokenizer.texts_to_sequences(text_data)
+
+print(word_index)
+# {'<OOV>': 1, 'today': 2, 'is': 3, 'a': 4, 'sunny': 5, 'day': 6, 'rainy': 7, 'it': 8}
+print(sequences)
+# [[2, 3, 4, 1, 6], [1, 8, 1, 7, 1]]
+# today is a <OOV> day
+# <OOV> itt <OOV> rainy <OOV>
+# 前者は本来の意味に近くなった
